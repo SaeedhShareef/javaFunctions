b1c0e3b5462b7d23a5ab40d19d8fe28f209104cd
Saeed Shareef <saeedhshareef@gmail.com>
2020-07-07 23:52:58 -0400

    javaFunctions

---
+++ js/app.js
@@ -0,0 +1,60 @@
+console.log('test run')
+
+x = 5
+
+y = 10
+
+function add(x,y){
+    return x + y ;
+}
+console.log(add(x+y))
+
+console.log(x)
+
+console.log(add)
+
+console.log(add(x,y))
+
+function subtract(x,y){
+    return y - x ;
+}
+
+console.log(subtract(y,x))
+
+function multiply(x,y){
+    return x * y;
+}
+console.log(multiply(x,y))
+
+function divide(x,y) {
+    return y / x;
+}
+console.log(divide(x,y))
+
+var a = [1,2,3];
+
+var b = [4,5,6];
+
+var c = [7,8,9];
+
+function arrayPush(a,x) {
+    arrayPush.push(a,5);
+}
+console.log(arrayPush)
+var a = [1,2,3];
+a.push(b,5)
+console.log(arrayPush(a,x))
+
+console.log(b)
+
+function newArray ([1,2,3], x) {
+    newArray.push(3);
+}
+
+console.log(newArray)
+
+var a = [1,2,3];
+    a.push(5);
+
+    console.log(a,5)
+
